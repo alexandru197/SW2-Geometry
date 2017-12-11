10be7fd0c92bba73711076709bc281dc244be80e
Nitescu Bogdan <bogdan.nitescu.16@ucl.ac.uk>
2017-12-11 23:22:06 +0000

    Fixex rotation and display.

--- src/Playground.java
+++ src/Playground.java
@@ -6,15 +6,15 @@ import java.util.ArrayList;
 public class Playground {
     public static void main (String[] args) {
 
-        Point point3 = new Point(0,0);
-        Point point4 = new Point(0,2);
-        Point point5 = new Point(1,1);
-//        Point point6 = new Point(1,-1);
+        Point point3 = new Point(1,2);
+        Point point4 = new Point(4,2);
+        Point point5 = new Point(4,3);
+        Point point6 = new Point(1,3);
         ArrayList<Point> coordinates = new ArrayList<Point>();
         coordinates.add(point3);
         coordinates.add(point4);
         coordinates.add(point5);
-//        coordinates.add(point6);
+        coordinates.add(point6);
         FurnitureObject furnitureObject = new FurnitureObject(coordinates,1);
         furnitureObject.displayCoordinates();
         furnitureObject.rotate(90);
